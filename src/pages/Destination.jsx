c78b6c3b35f14298e7e9a65ca58c1218df4e2fec
Wesley Tuschi <levituschi@gmail.com>
2023-09-15 23:31:02 -0300

    Destination funcional com css em desenvolvimento

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,25 +1,63 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
 import "../styles/Destination.css";
+import destinationData from "../data.json";
 
 const Destination = () => {
-    return (
-        <div className="destination vh-100">
-           <NavBar />
-        <div className="container">
-          <h2>So, you want to travel to</h2>
-          <h1>Space</h1>
-          <p>
-            Let’s face it; if you want to go to space, you might as well genuinely go to
-            outer space and not hover kind of on the edge of it. Well sit back, and relax
-            because we’ll give you a truly out of this world experience!
-          </p>
+  const [selectedPlanet, setSelectedPlanet] = useState("Moon");
+
+  const handlePlanetClick = (planetName) => {
+    setSelectedPlanet(planetName);
+  };
+
+  const planetInfo = destinationData.destinations.find(
+    (destination) => destination.name === selectedPlanet
+  );
+
+  console.log(planetInfo);
+
+  return (
+    <div className='destination vh-100'>
+      <NavBar />
+      <div className='container text-center row g-0 text-center'>
+        <div className='col-sm-6 .col-md-8'>
+          <h2>
+            <span>01</span> PICK YOUR DESTINATION
+          </h2>
+          {planetInfo && (
+            <img
+              src={require(`../${planetInfo.images.png}`)}
+              alt={planetInfo.name}
+              className="img-fluid"
+            />
+          )}
         </div>
-      
-        <button className="btn">Explore</button>
+        <div className='col-6 .col-md-4 infos'>
+          {destinationData.destinations.map((destination, index) => (
+            <span className="planets-names"
+              key={index}
+              onClick={() => handlePlanetClick(destination.name)}
+            >
+              {destination.name}
+            </span>
+          ))}
+          {planetInfo && (
+            <div>
+              <h1>{planetInfo.name}</h1>
+              <p className="description">{planetInfo.description}</p>
+              <div className='line-down'></div>
+              <div className='row row-cols-2 text-center infos'>
+                <h3 className='col'>AVG. DISTANCE</h3>
+                <h3 className='col'>EST. TRAVEL TIME</h3>
+                <p className='col dates'>{planetInfo.distance}</p>
+                <p className='col dates'>{planetInfo.travel}</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-      
-    );
-}
+    </div>
+  );
+};
 
 export default Destination;
